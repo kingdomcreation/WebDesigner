54f0c7d318c00f19fc206c9c1427af385e043cb9
kingdomcreation <kingdomcreation@gmail.com>
2018-06-01 16:29:45 -0400

    Add client to window to push into the cue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 console.log("main.js loaded");
 
+(function(w,k){
+    while( a = w[k].q.pop() ){
+        if( a[1] ){
+          var data = a[1];
+        };
+        if( a[0] === 'init'){
+          console.log('init was called')
+        };
+    }
+})(window,'client');
 function button_click(){
     console.log("button_click was called");
 }
