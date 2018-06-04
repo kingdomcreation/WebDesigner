c7163e1ba0903d8d344e157a0ae984eda173c70d
kingdomcreation <kingdomcreation@gmail.com>
2018-06-04 18:49:46 -0400

    Include CodeIgniter 3.1.8 system

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<?php define('PHP_','php/'); include('php/functions.php');
-debug();
-$index = $data['url'] = slug();
-$pages = [
-	'index'=>'Project name',
-	'about'=>'About',
-	'contact'=>'Contact'
-];
-if( isset($pages[$index])){
-	$index = $index;
-}else{
-	$index = 'index';
-}
-
-$title = $data['title'] = $pages[$index];
-index($data);
-
+<?php 
 /**
  * CodeIgniter
  *
