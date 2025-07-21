6c9db0288a8b1a23f9aea46539ab091690bac263
Kingdom Creation <kingdomcreation@gmail.com>
2025-07-21 12:14:15 -0400

    Add Bootstrap Icons

---
+++ js/simple-line-icons.js
@@ -0,0 +1,26 @@
+(function() {
+    var icons = [
+    'user','user-female','user-following','login','emotsmile','call-end',
+    'call-out','location-pin','directions','layers','list','options',
+    'arrow-left','arrow-up','arrow-left-circle','arrow-down-circle','clock',
+    'minus','event','organization','screen-smartphone','plane','mustache',
+    'magnet','disc','cursor-move','chemistry','shield','magic-wand','graduation',
+    'game-controller','eyeglass','envelope-letter','badge','wallet','speech',
+    'printer','playlist','picture','globe-alt','folder-alt','film','drop','docs',
+    'diamond','calculator','briefcase','basket-loaded','bag','action-redo',
+    'umbrella','tag','frame','size-actual','share-alt','rocket','pie-chart',
+    'note','home','graph','music-tone-alt','earphones-alt','equalizer','dislike',
+    'control-rewind','control-pause','control-end','volume-2','calendar','chart',
+    'bubble','camera','cloud-upload','eye','heart','key','lock','magnifier',
+    'magnifier-remove','paper-plane','refresh','settings','symbol-female','target',
+    'paypal','social-twitter','social-instagram','social-pinterest','social-google',
+    'social-skype','social-behance','social-soundcloud','social-stumbleupon',
+    'social-dropbox','social-steam'
+    ];
+    var length = icons.length;
+    
+    while (length--) {
+        var tag = icons[length];
+        console.log('icon-'+tag);
+    }
+})
